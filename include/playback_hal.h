a444c2a4ea8b3310f500bd21ad5a4c9c4b87bb6f
max_10 <max_10@gmx.de>
2021-10-30 19:36:41 +0200

    - cleanup gstreamer

--- include/playback_hal.h
+++ include/playback_hal.h
@@ -4,22 +4,14 @@
 #elif HAVE_SPARK_HARDWARE
 #include "../libspark/playback_libeplayer3.h"
 #elif HAVE_ARM_HARDWARE
-#if ENABLE_GSTREAMER_10
-#include "../libarmbox/playback_gst.h"
-#else
-#if BOXMODEL_HISILICON
-#include "../libarmbox/playback_hisilicon.h"
-#else
 #include "../libarmbox/playback_libeplayer3.h"
-#endif
-#endif
 #elif HAVE_MIPS_HARDWARE
 #include "../libmipsbox/playback_libeplayer3.h"
 #elif HAVE_GENERIC_HARDWARE
 #if BOXMODEL_RASPI
 #include "../libraspi/playback_lib.h"
 #else
-#if ENABLE_GSTREAMER_10
+#if ENABLE_GSTREAMER
 #include "../libgeneric-pc/playback_gst.h"
 #else
 #include "../libgeneric-pc/playback_lib.h"
